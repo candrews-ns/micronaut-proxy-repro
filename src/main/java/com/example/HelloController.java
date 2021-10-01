d6fbc5c1a69c1bc87a58926886e2a9448c70cf69
Andrews, Christopher <candrews@netsuite.com>
2021-10-01 13:07:43 +0100

    Micronaut 2.5.13 proxy-repro app.
    GET /blocking-first -> works, but blocks
    GET /flowable -> doesn't work, no body returned
    GET /single-or-error -> works, non-blocking? as used in CWS.

---
+++ src/main/java/com/example/HelloController.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2017 original authors
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ */
+package com.example;
+
+import io.micronaut.http.*;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.client.RxProxyHttpClient;
+import io.micronaut.http.uri.UriBuilder;
+import io.micronaut.validation.Validated;
+import io.reactivex.Flowable;
+import io.reactivex.Single;
+
+import javax.inject.Inject;
+import javax.validation.constraints.NotBlank;
+import java.net.URI;
+
+@Controller("/")
+@Validated
+public class HelloController {
+
+    @Inject
+    RxProxyHttpClient httpClient;
+
+    @Get(uri = "/blocking-first")
+    public HttpResponse<?> blockingFirst() {
+        URI uri = UriBuilder.of("http://localhost:8080/hello/foo").build();
+        MutableHttpRequest<?> req = HttpRequest.GET(uri);
+        return httpClient.proxy(req).blockingFirst();
+    }
+
+    @Get(uri = "/single-or-error")
+    public Single<MutableHttpResponse<?>> singleOrError() {
+        URI uri = UriBuilder.of("http://localhost:8080/hello/foo").build();
+        MutableHttpRequest<?> req = HttpRequest.GET(uri);
+        return httpClient.proxy(req).singleOrError();
+    }
+    
+    @Get(uri = "/flowable")
+    public Flowable<MutableHttpResponse<?>> flowable() {
+        URI uri = UriBuilder.of("http://localhost:8080/hello/foo").build();
+        MutableHttpRequest<?> req = HttpRequest.GET(uri);
+        return httpClient.proxy(req);
+    }
+
+    @Get(uri = "/hello/{name}", produces = MediaType.TEXT_PLAIN)
+    public Single<String> hello(@NotBlank String name) {
+        return Single.just("Hello " + name + "!");
+    }
+}
